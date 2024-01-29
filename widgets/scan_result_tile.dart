e4ef0848cd7789cdaca9f88d4405687549afbe68
santi-nue <68065589+santi-nue@users.noreply.github.com>
2024-01-29 13:45:49 +0100

    Update scan_result_tile.dart

--- widgets/scan_result_tile.dart
+++ widgets/scan_result_tile.dart
@@ -128,7 +128,8 @@ class _ScanResultTileState extends State<ScanResultTile> {
         _buildAdvRow(context, 'Complete Local Name', adv.localName),
         _buildAdvRow(context, 'Tx Power Level', '${adv.txPowerLevel ?? 'N/A'}'),
         _buildAdvRow(context, 'Manufacturer Data', getNiceManufacturerData(adv.manufacturerData)),
-        _buildAdvRow(context, 'Service UUIDs', getNiceServiceUuids(adv.serviceUuids)),
+        //_buildAdvRow(context, 'Service UUIDs', getNiceServiceUuids(adv.serviceUuids)),
+        _buildAdvRow(context, 'Service UUIDs', getNiceServiceUuids(adv.serviceUuids.map((key, value) => MapEntry(key.toString(), value)))),
         _buildAdvRow(context, 'Service Data', getNiceServiceData(adv.serviceData)),
       ],
     );
